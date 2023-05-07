852c99b95fcdbb71d67fae4d9c4b6eca64d69b79
aasimsyed97 <sayedaasim555@gmail.com>
2023-05-07 23:37:32 +0530

    project created

---
+++ BurgerBites/src/main/java/com/BurgerBites/BurgerBitesApplication.java
@@ -0,0 +1,14 @@
+package com.BurgerBites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgerBitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgerBitesApplication.class, args);
+		System.out.print("hello world");
+	}
+
+}
---
+++ BurgerBites/src/test/java/com/BurgerBites/BurgerBitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BurgerBites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgerBitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
