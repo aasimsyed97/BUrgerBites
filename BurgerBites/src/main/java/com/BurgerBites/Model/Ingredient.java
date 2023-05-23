f069c324c31edd21373f80e53dbdad4fa575b8ee
aasimsyed97 <sayedaasim555@gmail.com>
2023-05-23 22:08:36 +0530

    push

--- BurgerBites/src/main/java/com/BurgerBites/Model/Ingredient.java
+++ BurgerBites/src/main/java/com/BurgerBites/Model/Ingredient.java
@@ -9,6 +9,6 @@ public class Ingredient {
  private final String name;
  private final Type type;
  public static enum Type {
- WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE, Burger
+ WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
  }
 }
